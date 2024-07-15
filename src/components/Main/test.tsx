3c97d33ae2d31aaf3f7dc617ddf9b8e028eb5d99
Matheus Bronca <matheus.bronca@frncubo.com>
2024-07-15 14:59:24 -0300

    Initial jest tests

---
+++ src/components/Main/test.tsx
@@ -0,0 +1,20 @@
+import { render, screen } from '@testing-library/react'
+
+import Main from '.'
+describe('<Main />', () => {
+  it('should render the heading', () => {
+    // renderiza o component
+    const { container } = render(<Main />)
+
+    // busca o elemento e verifica a existência dele
+    expect(
+      screen.getByRole('heading', {
+        name: /react avançado/i
+      })
+    ).toBeInTheDocument()
+
+    expect(container.firstChild).toMatchSnapshot()
+  })
+
+  // gerar snapshot
+})
